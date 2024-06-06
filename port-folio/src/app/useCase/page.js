fa7306ab6f12c54c1a23293410728c20e76db4ff
DamienBlot <d.blot.28@gmail.com>
2024-06-06 16:21:17 +0200

    Create page.js

---
+++ port-folio/src/app/useCase/page.js
@@ -0,0 +1,9 @@
+import Card from "../(components)/card";
+
+export default function UseCase() {
+    return (
+      <div className="grid grid-cols-2 gap-8 justify-items-center  mb-10">
+        <h2 className=" col-span-2 text-5xl justify-self-center mt-10 mb-10">Use Case</h2>
+          <p className=" col-span-2">I use a trailhead sandbox for creating a user from a form then creating a account using a form as well. And be able to check their creation in the sandbox</p>
+        </div>
+    )};
